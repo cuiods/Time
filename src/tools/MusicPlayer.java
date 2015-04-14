062310f362c27805f6dac4fe278ac428a9fbf7e0
niansong1996 <niansong1996@163.com>
2015-04-14 22:18:25 +0800

    改了包名，加了音乐播放方法

---
+++ src/tools/MusicPlayer.java
@@ -0,0 +1,41 @@
+package tools;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class MusicPlayer {
+	public void play(String file, boolean loop) throws UnsupportedAudioFileException, IOException, LineUnavailableException{ 
+		do{
+		// 获取音频输入流
+		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(file));
+		// 获取音频编码对象
+		AudioFormat audioFormat = audioInputStream.getFormat(); 
+		// 设置数据输入
+		DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class,audioFormat, AudioSystem.NOT_SPECIFIED);
+		SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+		sourceDataLine.open(audioFormat);
+		sourceDataLine.start(); 
+		/*
+		 * 从输入流中读取数据发送到混音器
+		 */
+		int count;
+		byte tempBuffer[] = new byte[1024];
+		while ((count = audioInputStream.read(tempBuffer, 0, tempBuffer.length)) != -1) {
+			if (count > 0) {
+				sourceDataLine.write(tempBuffer, 0, count);
+			}
+		} 
+		// 清空数据缓冲,并关闭输入
+		sourceDataLine.drain();
+		sourceDataLine.close();
+		}while(loop);
+	}
+	} 
