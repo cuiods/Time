06676f678cff02609327a34ea246180ad39942cd
niansong1996 <niansong1996@163.com>
2015-05-10 14:26:25 +0800

    第三关科技有个bug，改好了

--- src/Tech/Tech_STG3_SecondCastle.java
+++ src/Tech/Tech_STG3_SecondCastle.java
@@ -4,6 +4,7 @@ import dataBase.DataBase;
 
 public class Tech_STG3_SecondCastle {
 	public Tech_STG3_SecondCastle(){
+		if(DataBase.Tech_SuperComputer = true)
 		DataBase.Tech_SecondCastle = true;
 	}
 }
