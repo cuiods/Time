fd18e53c6af13b831d673cc9443b38bd3fa54a08
niansong1996 <niansong1996@163.com>
2015-05-16 15:40:46 +0800

    刚刚的有问题，再来一次

--- src/Tech/Tech_ENM_STG3_LockScience.java
+++ src/Tech/Tech_ENM_STG3_LockScience.java
@@ -2,8 +2,6 @@ package Tech;
 
 import tools.Time;
 import dataBase.DataBase;
-
-<<<<<<< HEAD
 public class Tech_ENM_STG3_LockScience implements Runnable {
 	Time t;
 	public Tech_ENM_STG3_LockScience(){
@@ -11,13 +9,6 @@ public class Tech_ENM_STG3_LockScience implements Runnable {
 	}
 	public void execute(){
 		while(t.getRemainTime()>=0){
-=======
-public class Tech_ENM_STG3_LockScience {
-	public Time locktime;
-	public void execute(){
-		locktime = new Time(DataBase.LOCKSCIENCE_TIMELIMIT);
-		while(locktime.getRemainTime()>=0){
->>>>>>> origin/master
 			DataBase.LockScience = true;
 	    }
 		DataBase.LockScience = false;
--- src/Tech/Tech_STG3_Hubble.java
+++ src/Tech/Tech_STG3_Hubble.java
@@ -7,12 +7,9 @@ public class Tech_STG3_Hubble {
 	public Tech_STG3_Hubble(){
 		if(!DataBase.LockScience){
 			DataBase.Tech_Harper = true;
-<<<<<<< HEAD
 			DataBase.Tech_Harper_TIME++;
 			DataBase.Money-=DataBase.Tech_Harper_P;
 			PanelFight.isTech_9=true;
-=======
->>>>>>> origin/master
 		}
 	}
 }
